63996309fb7ada14c22ffbf161f0801d0b427f80
SuJianrong <1014150286@qq.com>
2024-07-16 20:25:45 +0800

    提交第8-13章代码

---
+++ C++/codes/2024-07/苏坚荣/C++PrimerPlus/Chapter12/12_4/stack.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include "stack.h"
+
+Stack::Stack(int n)
+{
+	size = n;
+	top = 0;
+	pitems = new Item[size];
+}
+
+Stack::Stack(const Stack& st)
+{
+	size = st.size;
+	top = st.top;
+	pitems = new Item[size];
+	for (int i = 0; i < st.size; i++) {
+		pitems[i] = st.pitems[i];
+	}
+}
+
+Stack::~Stack()
+{
+	delete[] pitems;
+}
+
+bool Stack::isEmpty() const
+{
+	return top == 0;
+}
+
+bool Stack::isFull() const
+{
+	return top == MAX;
+}
+
+bool Stack::push(const Item& item)
+{
+	if (top < MAX) {
+		pitems[top++] = item;
+		return true;
+	}
+	return false;
+}
+
+bool Stack::pop(Item& item)
+{
+	if (top > 0) {
+		item = pitems[--top];
+		return true;
+	}
+	return false;
+}
+
+Stack& Stack::operator=(const Stack& st)
+{
+	if (this == &st) {
+		return *this;
+	}
+	size = st.size;
+	top = st.top;
+	pitems = new Item[size];
+	for (int i = 0; i < st.size; i++) {
+		pitems[i] = st.pitems[i];
+	}
+}
---
+++ C++/codes/2024-07/苏坚荣/C++PrimerPlus/Chapter13/13_1/CD.cpp
@@ -0,0 +1,52 @@
+﻿#include "CD.h"
+#include <string.h>
+#include<iostream>
+
+Cd::Cd(const char* s1, const char* s2, int n, double x)
+{
+	strcpy_s(performers, 50, s1);
+	strcpy_s(label, 20, s2);
+	selections = n;
+	playtime = x;
+}
+
+Cd::Cd(const Cd& d)
+{
+	strcpy_s(performers, 50, d.performers);
+	strcpy_s(label, 20, d.label);
+	selections = d.selections;
+	playtime = d.playtime;
+}
+
+
+void Cd::Report() const
+{
+	using namespace std;
+	cout << "performers: " << performers << endl;
+	cout << "label: " << label << endl;
+	cout << "selections: " << selections << endl;
+	cout << "playtime: " << playtime << endl;
+}
+
+Cd& Cd::operator=(const Cd& d)
+{
+	if (this == &d) {
+		return *this;
+	}
+	strcpy_s(performers, 50, d.performers);
+	strcpy_s(label, 20, d.label);
+	selections = d.selections;
+	playtime = d.playtime;
+}
+
+Classic::Classic(const char* s, const char* s1, const char* s2, int n, double x) :Cd(s1, s2, n, x)
+{
+	strcpy_s(song, 50, s);
+}
+
+void Classic::Report() const
+{
+	using namespace std;
+	Cd::Report();
+	cout << "song: " << song << endl;
+}
